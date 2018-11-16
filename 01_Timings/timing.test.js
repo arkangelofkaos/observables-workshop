87f6747a0a5c7f9fe9f846d588c80219b2fda6b5
Edward Wong <edward.yueshungwong@timgroup.com>
2018-11-16 12:56:50 +0000

    adding test for a fixed rate observable

--- 01_Timings/timing.test.js
+++ 01_Timings/timing.test.js
@@ -1,20 +1,40 @@
-import Observable from "zen-observable";
 import "regenerator-runtime";
 
 function createDelayedObservable(delayMilliseconds) {
-    return new Observable(observer => {
-            // TODO("Create an observable which delays completion by the given delayMilliseconds")
-        }
-    );
+    // TODO("Create an observable which delays completion by the given delayMilliseconds")
 }
 
 xit("will delay completion by the given milliseconds", async () => {
     let specifiedDelay = 500;
 
     const started = new Date();
-    await createDelayedObservable(specifiedDelay).forEach(forceObservableToCompleteViaForEach => {});
+    await createDelayedObservable(specifiedDelay).forEach(forceObservableToCompleteViaForEach => {
+    });
     const ended = new Date();
     const actualDelay = ended.getTime() - started.getTime();
 
     return expect(actualDelay).toBeGreaterThan(specifiedDelay);
+});
+
+function createFixedRateObservable(rateMilliseconds) {
+    // TODO("Create an observable which outputs values every rateMilliseconds")
+}
+
+xit("will emit numbers every 500 milliseconds", async () => {
+    const desiredNumberOfValues = 2;
+
+    const started = new Date();
+    const regularValuesEmitted = await new Promise((resolve, reject) => {
+        const buffer = [];
+
+        createFixedRateObservable(500).subscribe(
+            (value) => {
+                // TODO("Add values to the buffer and resolve the promise once we have the desiredNumberOfValues")
+            });
+    });
+    const ended = new Date();
+    const actualDelay = ended.getTime() - started.getTime();
+
+    expect(regularValuesEmitted.length).toEqual(desiredNumberOfValues);
+    expect(actualDelay).toBeGreaterThan(1000);
 });
